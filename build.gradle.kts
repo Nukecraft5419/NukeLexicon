b86c73d3bc36424eaf871df17f9c784b412f4147
Nukecraft5419 <72621737+Nukecraft5419@users.noreply.github.com>
2026-03-06 17:18:26 +0100

    build: ♻️ refactor build logic to modern Gradle standards

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,16 +3,13 @@ plugins {
     id("maven-publish")
 }
 
-group = project.property("group") as String
-version = project.property("version") as String
+group = providers.gradleProperty("group").get()
+version = providers.gradleProperty("version").get()
 
 repositories {
     mavenCentral()
     maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/") {
         name = "spigotmc-repo"
-    }
-    maven("https://central.sonatype.com/repository/maven-snapshots/") {
-        name = "central-snapshots"
     }
     maven("https://repo.extendedclip.com/releases/") {
         name = "placeholder-api"
