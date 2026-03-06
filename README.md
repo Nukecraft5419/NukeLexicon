aed52fa10f66119e887f3d8847a375ef3535951f
Nukecraft5419 <72621737+Nukecraft5419@users.noreply.github.com>
2026-03-06 06:59:59 +0100

    build: 🎉 initial project setup

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # NukeLexicon
+A modern, lightweight i18n and MiniMessage parsing API for Spigot plugins.
---
+++ build.gradle.kts
@@ -0,0 +1,39 @@
+plugins {
+    id("java-library")
+    id("maven-publish")
+}
+
+group = project.property("group") as String
+version = project.property("version") as String
+
+repositories {
+    mavenCentral()
+    maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/") {
+        name = "spigotmc-repo"
+    }
+    maven("https://central.sonatype.com/repository/maven-snapshots/") {
+        name = "central-snapshots"
+    }
+    maven("https://repo.extendedclip.com/releases/") {
+        name = "placeholder-api"
+    }
+}
+
+dependencies {
+    compileOnly(libs.spigot) // Spigot API
+    compileOnly(libs.placeholderApi) // Placeholder API
+    compileOnly(libs.adventureBukkit) // Adventure Platform Bukkit
+    compileOnly(libs.miniMessage) // MiniMessage API
+
+    implementation(libs.bStats)
+}
+
+java {
+  toolchain {
+    languageVersion = JavaLanguageVersion.of(25)
+  }
+}
+
+tasks.withType<JavaCompile>().configureEach {
+  options.encoding = "UTF-8"
+}
